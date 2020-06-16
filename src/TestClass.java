3da7be471fc7240b25cfa2412fd47c3bead08b78
MBadak <67018566+MBadak@users.noreply.github.com>
2020-06-16 20:00:21 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Gitden merhaba!");
-
+         System.out.println("GitHub dan merhaba!");
     }
 }
